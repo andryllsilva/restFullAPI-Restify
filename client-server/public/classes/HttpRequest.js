dc9991148c79b381970e8cb113d20f3ac4ab436e
Andryll Silva <andryll.silva@outlook.com>
2025-01-31 15:34:29 -0300

    Erro ao fazer as rotas de put, post e delete

--- client-server/public/classes/HttpRequest.js
+++ client-server/public/classes/HttpRequest.js
@@ -35,8 +35,9 @@ class HttpRequest {
         resolve(obj)
       }
       
-
-      ajax.send()
+      ajax.setRequestHeader('Content-Type', 'application/json')
+    
+      ajax.send(JSON.stringify(params));
     })
   }
 }
